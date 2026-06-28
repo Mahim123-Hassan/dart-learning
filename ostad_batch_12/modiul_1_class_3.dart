f294d6a8d933c48e4d28fc85f2519557e0d870f6
Mahim123-Hassan <hmahim951@gmail.com>
2026-06-29 00:24:26 +0600

    added oparetor

--- ostad_batch_12/modiul_1_class_3.dart
+++ ostad_batch_12/modiul_1_class_3.dart
@@ -1,3 +1,5 @@
+
+
 void main (){
   var name ="Mahim hasan";
 
@@ -7,5 +9,16 @@ void main (){
   name1=10;
   print(name1.runtimeType);
 
-
+ String firstName='Mahim';
+ String lastname='hassan';
+ String fulname='$firstName $lastname';
+ String fulname1=fulname + ' '+ lastname;
+ print(fulname1);  
+ String fulname2 ='${firstName} ${lastname}';
+int a =10;
+int b=40;
+print('a+b=${a+b}');
+print('a-b=${a-b}');
+print('a*b=${a*b}');
+print('a/b=${a/b}');
 }
