a7a220af88ed9f75fed8410c91f9e526c2cb6bcf
Mahim123-Hassan <hmahim951@gmail.com>
2026-06-30 20:57:27 +0600

    added post increment

--- ostad_batch_12/modiul_1_class_3.dart
+++ ostad_batch_12/modiul_1_class_3.dart
@@ -1,29 +1,12 @@
 
-
 void main (){
-  var name ="Mahim hasan";
 
-  print(name);
-  dynamic name1='shahin';
-  print(name1.runtimeType);
-  name1=10;
-  print(name1.runtimeType);
-
- String firstName='Mahim';
- String lastname='hassan';
- String fulname='$firstName $lastname';
- String fulname1=fulname + ' '+ lastname;
- print(fulname1);  
- String fulname2 ='${firstName} ${lastname}';
-int a =10;
-int b=40;
-print('a+b=${a+b}');
-print('a-b=${a-b}');
-print('a*b=${a*b}');
-print('a/b=${a/b}');
 
 // post increment 
-a=10;
-print(a++);
+int a =5;
+int b=++a;
 print(a);
+print(b);
+
+
 }
